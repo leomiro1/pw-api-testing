8ef6ce45c11ed37e3d6aaa9274a36fe08f56ac0f
Leo Miro <leomiro1@yahoo.com>
2026-03-02 16:00:57 -0300

    adding html modification on config

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: [['html'],['list']],
+  reporter: [['html', {open: 'never'}],['list']],
   use: {
     trace: 'retain-on-failure'
   },
